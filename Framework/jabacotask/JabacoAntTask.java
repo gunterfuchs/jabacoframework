dcd38fad009afbb8524d3e7dc04e7d852ab935db
Günter Fuchs <gfuchs@gmail.com>
2025-01-22 14:31:21 -0700

    Added fixing files for the VB interface classes to the Ant task.

--- Framework/jabacotask/JabacoAntTask.java
+++ Framework/jabacotask/JabacoAntTask.java
@@ -1,7 +1,12 @@
 import org.apache.tools.ant.Task;
 import org.apache.tools.ant.BuildException;
 import java.util.Vector;
-import java.util.Iterator;
+import java.nio.file.Files;
+import java.nio.file.Path;
+//import java.io.IOException;
+import java.io.File;
+//import java.nio.file.Files;
+//import java.util.Iterator;
 
 public class JabacoAntTask extends Task {
 
@@ -9,6 +14,13 @@ public class JabacoAntTask extends Task {
     String message;
     public void setMessage(String msg) {
         message = msg;
+    }
+
+    /** the output folder for the VB class files, as attribute */
+    Path vbPath;
+    public void setVbPath(Path vbPath) {
+    	//System.out.print(vbPath.toString());
+    	this.vbPath = vbPath;
     }
 
     /** Should the build fail? Defaults to false. As attribute. */
@@ -25,29 +37,71 @@ public class JabacoAntTask extends Task {
     /** Do the work. */
     public void execute() {
         // handle attribute 'fail'
-        if (fail) throw new BuildException("Fail requested.");
+        if (fail) {
+        	throw new BuildException("Fail requested.");
+        }
 
         // handle attribute 'message' and nested text
-        if (message!=null)  {
+        if (message != null)  {
         	log(message);
+        }
+        if (vbPath != null) {
+        	// In interface class files, replace 0x0201 with 0x0601.
+        	//log(String.format("Path to VB class files is %s.", vbPath.toString()));
+        	try {
+	        	File dir = new File(vbPath.toString());
+	        	File[] files = dir.listFiles((d, name) -> name.startsWith("I"));
+	        	for (File file : files) {
+	        		System.out.println(file.toString());
+	        		byte[] classAsBinary = Files.readAllBytes(file.toPath());
+	        		final byte[] SEARCH = new byte[] {0x02, 0x01};
+	        		final byte REPLACE = 0x06;
+        		    boolean find = false;
+        		    int i;
+        		    for (i = 0; i <= (classAsBinary.length - SEARCH.length); i++) {
+        		        if (classAsBinary[i] == SEARCH[0]) {
+        		            find = true;
+        		            for (int j = 0; j < SEARCH.length; j++) {
+        		                if (classAsBinary[i + j] != SEARCH[j]) {
+        		                    find = false;
+        		                }
+        		            }
+        		        }
+        		        if (find) {
+        		            break;
+        		        }
+        		    }
+        		    if (find) {
+        		    	System.out.println(String.format("%s: Found access tag at position %d.", file.toString(), i));
+        		    }
+        		    else {
+    	        		System.out.println(String.format("%s: Could not find access tag.", file.toString()));
+        		    }
+	        		// Replace 0x0201 with 0x0601.
+        		    classAsBinary[i] = REPLACE;
+        		    Files.write(file.toPath(), classAsBinary);
+        		}
+	        }
+        	catch(Exception ex) {
+        		System.err.print(ex.getMessage());
+        	}
         }
 
         // handle nested elements
-        for (Iterator it=messages.iterator(); it.hasNext(); ) {
-            Message msg = (Message)it.next();
-            log(msg.getMsg());
-        }
+//        for (Iterator it=messages.iterator(); it.hasNext(); ) {
+//            Message msg = (Message)it.next();
+//            log(msg.getMsg());
+//        }
     }
 
 
     /** Store nested 'message's. */
-    Vector messages = new Vector();
+    //Vector messages = new Vector();
 
     /** Factory method for creating nested 'message's. */
-    public Message createMessage() {
-        Message msg = new Message();
-        messages.add(msg);
-        return msg;
-    }
-
+//    public Message createMessage() {
+//        Message msg = new Message();
+//        messages.add(msg);
+//        return msg;
+//    }
 }
